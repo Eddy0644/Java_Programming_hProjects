f6994b2a53be5665378606fe7615980ea0b9177c
netcy <a@nc32.top>
2023-04-14 11:12:55 +0800

    not end today's java lesson

--- sy1/src/Fract.java
+++ sy1/src/Fract.java
@@ -6,17 +6,21 @@ public class Fract {
         this.num = a;
         this.den = b;
     }
-
+    //This program is not optimised for operations on numbers below zero.
+    //So using negative numbers in examples are discouraged.
     public static void main(String[] args) {
-//        System.out.println(getMinMultiple(5, 6));
-//        System.out.println(getMaxDivisor(15, 25));
+        System.out.println("Source is: 1/5 and 7/20.");
         Fract f1 = new Fract(1, 5), f2 = new Fract(7, 20), f3;
+        System.out.println("Results for {Add,Sub,Multiple,Divide}:");
         f3 = f1.add(f2);
         f3.show();
+        f1.sub(f2).show();
+        f1.multiple(f2).show();
+        f1.divide(f2).show();
     }
 
     public static int getMaxDivisor(int m, int n) {
-        for (int i = (int) (m / 2); i > 1; i--) {
+        for (int i = m / 2; i > 1; i--) {
             if (m % i == 0 && n % i == 0) return i;
         }
         return 1;
@@ -39,7 +43,31 @@ public class Fract {
         return new Fract(new_num / r, mm / r);
     }
 
+    public Fract sub(Fract f) {
+        int mm = getMinMultiple(this.den, f.den);
+        int new_num = (mm / this.den) * this.num - (mm / f.den) * f.num;
+        int r = getMaxDivisor(new_num, mm);
+        return new Fract(new_num / r, mm / r);
+    }
+
+    public Fract multiple(Fract f) {
+        Fract p = new Fract(this.num * f.num, this.den * f.den);
+        // 结果分数的分子分母可同整除的最大整数
+        int r = getMaxDivisor(p.num, p.den);
+        p.num /= r;
+        p.den /= r;
+        return p;
+    }
+
+    public Fract divide(Fract f) {
+        Fract p = new Fract(this.num * f.den, this.den * f.num);
+        int r = getMaxDivisor(p.num, p.den);
+        p.num /= r;
+        p.den /= r;
+        return p;
+    }
+
     public void show() {
-        System.out.printf("%d/%d", this.num, this.den);
+        System.out.printf("[%3d/%3d]\n", this.num, this.den);
     }
 }
