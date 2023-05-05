afec0c0c11cf174f1328c706a5304f01b99f99ae
netcy <a@nc32.top>
2023-05-05 11:20:26 +0800

    √

--- sy3/src/ShapeUnitTest.java
+++ sy3/src/ShapeUnitTest.java
@@ -1,9 +1,21 @@
 public class ShapeUnitTest {
     public static void main(String[] args) {
+        String outputTemplate = "%8s: S=%.2f L=%.2f \n";
+
         Circle circle = new Circle(0, 0, 1);
-        System.out.printf("%8s: S=%.2f L=%.2f \n", "Circle", circle.getArea(), circle.getLength());
+        System.out.printf(outputTemplate, "Circle", circle.getArea(), circle.getLength());
+
         Triangle triangle = new Triangle(0, 0, new Point(3, 0), new Point(3, 4));
-        System.out.printf("%8s: S=%.2f L=%.2f \n", "Triangle", triangle.getArea(), triangle.getLength());
+        System.out.printf(outputTemplate, "Triangle", triangle.getArea(), triangle.getLength());
+
+        Quadrant quadrant = new Quadrant(0, 0, new Point(3, 0), new Point(3, 4), new Point(0, 4));
+        System.out.printf(outputTemplate, "Quadrant", quadrant.getArea(), quadrant.getLength());
+
+        Pentagon pentagon = new Pentagon(0, 0, new Point(4, 0), new Point(4, 2), new Point(2, 4), new Point(0, 4));
+        System.out.printf(outputTemplate, "Pentagon", pentagon.getArea(), pentagon.getLength());
+
+        Elliptic elliptic = new Elliptic(0, 0, 3, 2);
+        System.out.printf(outputTemplate, "Elliptic", elliptic.getArea(), elliptic.getLength());
     }
 }
 
@@ -152,13 +164,23 @@ class Elliptic extends Circle {
 }
 
 class Triangle extends Polygon {
-    int n = 3;
+    final int n = 3;
     Point p1, p2, p3;
     double a, b, c;
 
     public Triangle(int x, int y, Point p2, Point p3) {
         super(x, y);
         this.p1 = new Point(x, y);
+        this.p2 = p2;
+        this.p3 = p3;
+        this.a = new Line(p1, p2).getLength();
+        this.b = new Line(p3, p2).getLength();
+        this.c = new Line(p1, p3).getLength();
+    }
+
+    public Triangle(Point p1, Point p2, Point p3) {
+        super(p1.getX(), p1.getY());
+        this.p1 = p1;
         this.p2 = p2;
         this.p3 = p3;
         this.a = new Line(p1, p2).getLength();
@@ -177,5 +199,69 @@ class Triangle extends Polygon {
 
     int getInnerAngles() {
         return 180;
+    }
+}
+
+class Quadrant extends Polygon {
+    final int n = 4;
+    Point p1, p2, p3, p4;
+    double a, b, c, d;
+
+    public Quadrant(int x, int y, Point p2, Point p3, Point p4) {
+        super(x, y);
+        this.p1 = new Point(x, y);
+        this.p2 = p2;
+        this.p3 = p3;
+        this.p4 = p4;
+        this.a = new Line(p1, p2).getLength();
+        this.b = new Line(p3, p2).getLength();
+        this.c = new Line(p4, p3).getLength();
+        this.d = new Line(p4, p1).getLength();
+    }
+
+    double getArea() {
+//        double p = (a + b + c) / 2;
+//        return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+        return new Triangle(p1, p2, p3).getArea() + new Triangle(p1, p3, p4).getArea();
+    }
+
+    double getLength() {
+        return a + b + c + d;
+    }
+
+    int getInnerAngles() {
+        return 360;
+    }
+}
+
+// 1 2 3; 1 3 5; 3 4 5
+class Pentagon extends Polygon {
+    final int n = 5;
+    Point p1, p2, p3, p4, p5;
+    double a, b, c, d, e;
+
+    public Pentagon(int x, int y, Point p2, Point p3, Point p4,Point p5) {
+        super(x, y);
+        this.p1 = new Point(x, y);
+        this.p2 = p2;
+        this.p3 = p3;
+        this.p4 = p4;
+        this.p5 = p5;
+        this.a = new Line(p1, p2).getLength();
+        this.b = new Line(p3, p2).getLength();
+        this.c = new Line(p4, p3).getLength();
+        this.d = new Line(p4, p5).getLength();
+        this.e = new Line(p1, p5).getLength();
+    }
+
+    double getArea() {
+        return new Triangle(p1, p2, p3).getArea() + new Triangle(p1, p3, p5).getArea()+ new Triangle(p3, p4, p5).getArea();
+    }
+    double getLength() {
+        return a + b + c + d+e;
+    }
+
+    int getInnerAngles() {
+        return 540;
     }
 }
