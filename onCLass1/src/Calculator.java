59540fa80d583f81c6e8ab04bcb265d0ad2f2eda
netcy <a@nc32.top>
2023-05-12 09:34:24 +0800

    .

--- onCLass1/src/Calculator.java
+++ onCLass1/src/Calculator.java
@@ -18,41 +18,14 @@ public class Calculator {
         win.setVisible(true);
         win.setLocation(200, 200);
         win.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        ActionListener btnListener = e -> {
-            hitCount++;
-            request.append(((JButton) e.getSource()).getText());
-//                System.out.print("Hit.\n"+((JButton)e.getSource()).getText());
-//                btn1.setText("Hit me! x"+hitCount);
-//                btn1.setBounds(20,20,100,60);
-            System.out.println(request);
-        };
-        JButton btn1 = new JButton("+");
-        btn1.setBounds(20, 20, 100, 60);
-        btn1.addActionListener(btnListener);
-
         JPanel contentPane = new JPanel();
+        contentPane.setLayout(new FlowLayout(FlowLayout.CENTER));
 
-        JButton[] btnGroup = new JButton[20];
-//        contentPane.setLayout(new BorderLayout());
-//        contentPane.setLayout(new GridLayout());
-        contentPane.setLayout(new FlowLayout());
-        String template = ".+-*/=";
-        for (int i = 0; i < 10; i++) {
-            btnGroup[i] = new JButton(String.valueOf(i));
-            btnGroup[i].addActionListener(btnListener);
-            contentPane.add(btnGroup[i]);
-        }
-        for (int i = 10; i < 10 + template.length(); i++) {
-            btnGroup[i] = new JButton(String.valueOf(template.charAt(i - 10)));
-            btnGroup[i].addActionListener(btnListener);
-            contentPane.add(btnGroup[i]);
-        }
-//        win.setContentPane(contentPane);
         JTextField tf1 = new JTextField();
         tf1.getDocument().addDocumentListener(new DocumentListener() {
             public void changedUpdate(DocumentEvent e) {
                 // Handle change events
-                System.out.println(e.toString());
+//                System.out.println(e.toString());
             }
 
             public void insertUpdate(DocumentEvent e) {
@@ -62,18 +35,38 @@ public class Calculator {
 
             public void removeUpdate(DocumentEvent e) {
                 // Handle remove events
-                System.out.println(e.toString());
+//                System.out.println(e.toString());
             }
         });
-
         tf1.setBounds(50, 50, 200, 30);
         tf1.setPreferredSize(new Dimension(200, 30));
         contentPane.add(tf1);
 
+
+        JButton[] btnGroup = new JButton[20];
+
+        ActionListener btnListener = e -> {
+            hitCount++;
+            request.append(((JButton) e.getSource()).getText());
+            tf1.setText(request.toString());
+            System.out.println(request);
+        };
+        JButton btn1 = new JButton("+");
+        btn1.setBounds(20, 20, 100, 60);
+        btn1.addActionListener(btnListener);
+
+
+        String template = "+-.*/=1234567890";
+        for (int i = 0; i < template.length(); i++) {
+            btnGroup[i] = new JButton(String.valueOf(template.charAt(i)));
+            btnGroup[i].addActionListener(btnListener);
+            btnGroup[i].setPreferredSize(new Dimension(63, 30));
+            contentPane.add(btnGroup[i]);
+        }
+
         win.getContentPane().add(contentPane);
         win.pack();
-
-//        win.add(btn1);
+        win.setSize(250, 300);
 
 
     }
