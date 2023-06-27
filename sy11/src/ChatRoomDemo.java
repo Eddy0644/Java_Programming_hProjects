d14cf6adae723a5146f75ac2dd4889ce3cfae27e
netcy <a@nc32.top>
2023-06-27 19:36:58 +0800

    .

--- sy11/src/ChatRoomDemo.java
+++ sy11/src/ChatRoomDemo.java
@@ -10,7 +10,8 @@ import java.util.ArrayList;
 
 public class ChatRoomDemo {
     public static void main(String[] args) {
-        new ChatRoomServer("Main", 23001);
+        new ChatRoomServer("SRV - 0", 23001);
+        new ChatRoomClient("CLI - 1", 23001, 1);
     }
 }
 
@@ -34,7 +35,7 @@ class ChatRoomServer extends JFrame {
         new Thread(new ServerThread()).start();
         textArea.append("Server is online.\n");
 
-        this.setSize(380, 452);
+        this.setSize(380, 470);
         this.setVisible(true);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
@@ -49,7 +50,10 @@ class ChatRoomServer extends JFrame {
         Container contentPane = this.getContentPane();
         contentPane.setLayout(new FlowLayout());
 
-        textArea = new JTextArea(20, 30);
+        textArea = new JTextArea(18, 36);
+        Font testfont = new Font("Consolas", Font.PLAIN, 16);
+        textArea.setFont(testfont);
+
         JScrollPane scrollPane = new JScrollPane(textArea);
         scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
 //        scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
@@ -132,8 +136,97 @@ class ChatRoomServer extends JFrame {
     }
 }
 
-class ChatRoomClient {
-    public ChatRoomClient() {
+class ChatRoomClient extends JFrame {
+    String hostname;
+    JTextArea textArea;     // For dual-direction messages
+    JTextField textField;   // Message waiting to be sent
+    PrintWriter pw;
+    Socket socket;
 
+    public ChatRoomClient(String name, int port, int pos) {
+        super("ChatClient " + name);
+        this.hostname = name;
+        paintComponent();
+
+        connectWithServer(port);
+        new Thread(new ReceiveThread(socket)).start();
+
+
+        this.setBounds(380 * pos, 0, 380, 470);
+        this.setVisible(true);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    private void connectWithServer(int port) {
+        try {
+            socket = new Socket("127.0.0.1", port);
+            textArea.append(this.hostname + " Connected With Server!\n");
+            pw = new PrintWriter(this.socket.getOutputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void paintComponent() {
+        Container contentPane = this.getContentPane();
+        contentPane.setLayout(new FlowLayout());
+
+        textArea = new JTextArea(18, 36);
+        Font testfont = new Font("Consolas", Font.PLAIN, 16);
+        textArea.setFont(testfont);
+
+        JScrollPane scrollPane = new JScrollPane(textArea);
+        scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+//        scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        contentPane.add(scrollPane);
+
+        textField = new JTextField();
+        textField.setPreferredSize(new Dimension(350, 30));
+        contentPane.add(textField);
+
+        JButton sendBtn = new JButton("Send");
+        sendBtn.addActionListener(e -> {
+            String msg = textField.getText();
+            // As our message would also appear in server broadcast, comment this to avoid duplicate.
+//            textArea.append(hostname + ": " + msg + "\n");
+            textField.setText("");
+            pw.println(hostname + ": " + msg);
+            pw.flush();
+        });
+        JButton clearBtn = new JButton("Clear");
+        clearBtn.addActionListener(e -> textArea.setText(""));
+
+        contentPane.add(sendBtn);
+        contentPane.add(clearBtn);
+    }
+
+    class ReceiveThread implements Runnable {
+        BufferedReader br;  //负责读出数据
+        private Socket client;
+
+        public ReceiveThread(Socket client) {
+            this.client = client;
+            try {
+                InputStreamReader isr = new InputStreamReader(this.client.getInputStream());
+                br = new BufferedReader(isr);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void run() {
+            while (true) {
+                //接收数据
+                try {
+                    String msg = br.readLine();
+                    if (!"".equals(msg.trim())) {
+                        textArea.append(msg + "\n");
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
